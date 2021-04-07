6eef9e3ffe8a27eb34fd043b6862b2b91e062349
Tela Caul <telacaul@gmail.com>
2021-04-06 19:49:40 -0700

    restructure folders

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
+const generateReadme = require("./generateREADME")
 const writeFileAsync = util.promisify(fs.writeFile);
 
 //Prompt the user questions to populate the README.md
